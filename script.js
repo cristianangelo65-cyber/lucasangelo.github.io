2e2b4ffea80d9d5c00e93479a774ba324d76422c
cristianangelo65-cyber <cristianangelo65@gmail.com>
2026-01-02 18:22:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+function salvarCarreira() {
+  const nome = document.getElementById("nome")?.value;
+  const posicao = document.getElementById("posicao")?.value;
+  const idade = document.getElementById("idade")?.value;
+
+  if (!nome) {
+    alert("Digite o nome do jogador");
+    return;
+  }
+
+  localStorage.setItem("nome", nome);
+  localStorage.setItem("posicao", posicao);
+  localStorage.setItem("idade", idade);
+
+  window.location.href = "gestao.html";
+}
